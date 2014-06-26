eb61009fe3bff83ceafa43362a98f4c75aa47fd2
Thijs Van der Schaeghe <thijs@catlab.be>
2014-06-26 15:29:19 +0200

    Initial commit.

--- php/config.php
+++ php/config.php
@@ -1,13 +1,39 @@
 <?php
 
 // Load database settings
+// This file has been specifically written for cloudwalkers.
+// Make sure to change these values in case you host this somewhere else.
+
 $dbfile = dirname (dirname (dirname (__FILE__))) . '/includes/db.php';
 
 if (file_exists ($dbfile))
 {
-	echo 'db file found (' . $dbfile . ')';
+	require $dbfile;
+
+	define('DB_HOST', 'localhost');
+	define('DB_USERNAME', 'myuser');
+	define('DB_PASSWORD', 'myuser');
+	define('DB_NAME', 'cloudwalkers');
 }
-else
+
+define('DB_CHARSET', 'utf8');
+
+if (!defined ('DB_HOST'))
 {
-	echo 'db file not found (' . $dbfile . ')';
+	define ('DB_HOST', 'localhost');
+}
+
+if (!defined ('DB_USERNAME'))
+{
+	define ('DB_USERNAME', 'myuser');
+}
+
+if (!defined ('DB_PASSWORD'))
+{
+	define ('DB_PASSWORD', 'myuser');
+}
+
+if (!defined ('DB_NAME'))
+{
+	define ('DB_NAME', 'cloudwalkers_order');
 }
