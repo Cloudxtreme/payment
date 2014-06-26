24d51d5b0da8e012dead7de5f67ed709eace2181
Thijs Van der Schaeghe <thijs@catlab.be>
2014-06-26 15:30:57 +0200

    Initial commit.

--- php/config.php
+++ php/config.php
@@ -8,12 +8,12 @@ $dbfile = dirname (dirname (dirname (__FILE__))) . '/includes/db.php';
 
 if (file_exists ($dbfile))
 {
-	require $dbfile;
+	require_once $dbfile;
 
-	define('DB_HOST', 'localhost');
-	define('DB_USERNAME', 'myuser');
-	define('DB_PASSWORD', 'myuser');
-	define('DB_NAME', 'cloudwalkers');
+	define('DB_HOST', $dbud["username"]);
+	define('DB_USERNAME', $dbud["password"]);
+	define('DB_PASSWORD', $dbud["server"]);
+	define('DB_NAME', $dbud["dbname"]);
 }
 
 define('DB_CHARSET', 'utf8');
