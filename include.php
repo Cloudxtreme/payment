c39bb9dff0b293dcffe3f7050cf8c5df90bfee6b
Thijs Van der Schaeghe <thijs@catlab.be>
2014-06-26 14:39:53 +0200

    Initial commit.

---
+++ include.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file offers the order flow to be included in an existing website.
+ */
+
+function cw_get_order_form_html ()
+{
+	return '<p>Hello world, this is an order form.</p>';
+}
